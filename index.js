076c127a7363b839d8643637db05958fa626bca9
omarbougarne <omarbougarne98@gmail.com>
2024-11-06 14:53:32 +0100

    Added att and func to div

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function addClass(){
     const text = document.createTextNode("attribute")
 
     label.appendChild(text)
-    mainDiv.appendChild(label)
+    div.appendChild(label)
 
     const input = document.createElement("input")
     input.setAttribute('type', 'text')
