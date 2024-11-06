f16d8dd2ab0bc3b08c9149d1d3aaad3c1f9f6623
omarbougarne <omarbougarne98@gmail.com>
2024-11-06 11:52:06 +0100

    code seperation

---
+++ index.js
@@ -0,0 +1,54 @@
+function addClass(){
+    const mainDiv = document.getElementById('class')
+    const div = document.createElement("div")
+    const text = document.createTextNode("class")
+
+    div.appendChild(text)
+    mainDiv.appendChild(div)
+
+    const button = document.createElement("button")
+    button.textContent = 'Attribute'
+    div.appendChild(button)
+
+    
+    
+    const buttonf = document.createElement("button")
+    buttonf.textContent = 'Functionalities'
+    div.appendChild(buttonf)
+
+    const input = document.createElement("input")
+    input.setAttribute('type', 'text')
+    input.setAttribute('placeholder', 'attribute')
+
+    div.appendChild(input)
+
+
+    //attribute
+    button.onclick = function addAtt(){
+    const label = document.createElement("label")
+    const text = document.createTextNode("attribute")
+
+    label.appendChild(text)
+    mainDiv.appendChild(label)
+
+    const input = document.createElement("input")
+    input.setAttribute('type', 'text')
+    input.setAttribute('placeholder', 'text')
+
+    label.appendChild(input)
+    }
+    //functions
+    buttonf.onclick = function addFunc(){
+        const label = document.createElement("label")
+        const text = document.createTextNode("funcitonalitie")
+
+        label.appendChild(text)
+        div.appendChild(label)
+
+    const input = document.createElement("input")
+    input.setAttribute('type', 'text')
+    input.setAttribute('placeholder', 'functionalities')
+
+    label.appendChild(input)
+    }
+   }
